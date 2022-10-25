1bd2fe2ae2a59accec958ed4f328562fbdd8aa6e
Veridiana Maricato <maricato.veridiana@gmail.com>
2022-10-25 11:54:05 -0300

    arrumei bug do jquery e finalizei footer

--- js/home.js
+++ js/home.js
@@ -1,5 +1,19 @@
+//Javascript puro
+
+// let menuButton = document.querySelector('.menu-principal__btn')
+// let menuPrincipal = document.querySelector(".menu-principal")
+
+// menuButton.addEventListener('click', abreFechaMenu);
+
+// function abreFechaMenu(){
+// menuPrincipal.classList.toggle("menu-principal--fechado")
+// }
+
 // jquery
-$(document).ready(function(){ 
+$(document).ready(function(){
+  
+
+    // Slide depoimentos
     $('.depoimentos__caixa').slick({
         autoplay:true,
         arrows: false,
