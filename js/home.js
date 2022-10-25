8eabc7efea2c666811e015c1b116b3ead9322432
Veridiana Maricato <maricato.veridiana@gmail.com>
2022-10-25 10:28:29 -0300

    fiz o js, mas esta com bug na parte do slick

---
+++ js/home.js
@@ -0,0 +1,9 @@
+// jquery
+$(document).ready(function(){ 
+    $('.depoimentos__caixa').slick({
+        autoplay:true,
+        arrows: false,
+        dots: true,
+        autoplaySpeed: 3000,
+    });
+})
