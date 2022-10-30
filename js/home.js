94842c9608e16af07ca9ecb7645b15b139b23a66
Veridiana Maricato <maricato.veridiana@gmail.com>
2022-10-30 14:23:38 -0300

    arrumei depoimentos, porem nao estao automaticos

--- js/home.js
+++ js/home.js
@@ -96,7 +96,7 @@ function checkMandatoryInputs(mandatoryInput) {
   // Ir para endereço
 
   function irParaEndereco(){
-    window.location.replace("/endereco.html")    
+    window.location.href = "/endereco.html" 
   }
 
 
@@ -164,6 +164,52 @@ function checarEIrParaPagamento(){
 
 }
 
+// DEPOIMENTOS
+const controls = document.querySelectorAll('.control');
+let currentItem = 0
+const items = document.querySelectorAll('.depoimentos__item');
+const maxItems = items.length
+controls.forEach((control) => {
+  control.addEventListener('click', () => {
+    const isLeft = control.classList.contains('arrow-left')
+
+    if (isLeft) {
+      currentItem -= 1
+    }else{
+      currentItem += 1
+    }
+
+    if(currentItem >= maxItems){
+      currentItem = 0
+    }
+    if(currentItem < 0){
+      currentItem = maxItems -1
+    }
+
+    items.forEach(item => item.classList.remove('depoimentos__item__current'))  
+    items[currentItem].scrollIntoView({
+      block: "center",
+      inline: "center"
+    }) 
+    items[currentItem].classList.add('depoimentos__item__current')
+   
+  })
+})
+
+let count = 1;
+document.getElementById('arrow-right').clicked = true;
+
+setInterval( function(){
+  nextImage();
+}, 2000)
+
+function nextImage(){
+  count++
+  if(count>4){
+    count = 1
+  }
+  document.getElementById('arrow-right'+count).clicked = true
+}
 
 // conferir endereco
 
